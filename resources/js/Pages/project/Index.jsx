bf6d712f7deff672864dfbc59cb8f56a151be348
Giorgi Akhobadze <axobadzegiorgi5@gmail.com>
2024-06-26 17:47:52 +0400

    Pass Project data to layout

---
+++ resources/js/Pages/project/Index.jsx
@@ -0,0 +1,25 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
+import {Head} from "@inertiajs/react";
+
+export default function Index({ auth, projects }) {
+    return(
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    Project
+                </h2>
+            }
+        >
+            <Head title="Projects" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">Projects</div>
+                        <pre>{JSON.stringify(projects, undefined, 2)}</pre>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    )
+}
